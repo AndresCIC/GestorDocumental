80b970d4e83ad9ace937905f2519589c8dc124e6
juancarrascocic <juancarrascocic@gmail.com>
2017-09-01 18:20:32 +0200

    Modificado el root.vue

--- cliente/components/root.vue
+++ cliente/components/root.vue
@@ -3,20 +3,20 @@
 		<div id = "Menu">
 			<ul class="nav nav-tabs" id="myTab" role="tablist">
 			  <li class="nav-item">
-				<a id='Usuarios' role="tab" data-toggle="tab" href="#Documentos" class="nav-link active" :class ="anchorClass('Documentos')" v-on:click="chooseMenu('Documentos')">Documentos</a>
+				<a id='Usuarios' role="tab" data-toggle="tab" href="#Documentos" class="nav-link active"  v-on:click="chooseMenu('Documentos')">Documentos</a>
 			  </li>
 			  <li class="nav-item">
-				 <a class="nav-link" data-toggle="tab" href="#Plantillas" role="tab" :class ="anchorClass('Plantillas')" v-on:click="chooseMenu('Plantillas')">Plantillas</a>
+				 <a class="nav-link" data-toggle="tab" href="#Plantillas" role="tab" v-on:click="chooseMenu('Plantillas')">Plantillas</a>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" data-toggle="tab" href="#Perfiles" role="tab":class ="anchorClass('Perfiles')" v-on:click="chooseMenu('Perfiles')">Perfiles de usuarios</a>
+				<a class="nav-link" data-toggle="tab" href="#Perfiles" role="tab" v-on:click="chooseMenu('Perfiles')">Perfiles de usuarios</a>
 			  </li>
 			</ul>				
 
 		</div>
 		<hr>
 		<div class="row">
-			<master :menuChoice="menuChoice" class="col-8"></master>
+			<master :menuChoice="menuChoice" class="col-12"></master>
 			<detail :menuChoice="menuChoice" :currentObject="currentObject"  :previousObject="previousObject" class="col"></detail>
 
 		</div>
@@ -44,7 +44,9 @@
 
 		},
 		methods:{
-			
+			anchorClass(tipo){
+
+			}
 		}
 	}
 </script>
