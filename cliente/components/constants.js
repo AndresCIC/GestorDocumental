dcad4c3515c2cd4c5c26bae456d2541a201612cd
juancarrascocic <juancarrascocic@gmail.com>
2017-09-01 17:38:31 +0200

    Subida estructura de cliente

---
+++ cliente/components/constants.js
@@ -0,0 +1,5 @@
+export default{
+			STATE_NEW : 0,
+			STATE_READ : 1,
+			STATE_UPDATE : 2
+}
---
+++ cliente/components/root.vue
@@ -0,0 +1,56 @@
+<template>
+	<div id="Root" class="root-div">
+		
+	</div>
+</template>
+
+<script>
+	import master from './master.vue'
+	import constantes from './constants.js'
+
+
+	export default{
+		components:{
+			tabs
+		},
+		data (){
+			return{
+			}
+		},
+		computed:{
+			showDetailEntradas: function(){
+				return this.showDetail && this.menuChoice == "Entrada";
+			},
+			showDetailPeliculas: function(){
+				return this.showDetail && this.menuChoice == "Pelicula";
+			},
+		},
+		methods:{
+			setNew(newState){
+				this.showDetail = true;
+				this.enableButtons = false;
+				this.read = false;	
+				this.detailMode = constantes.STATE_NEW;
+			},
+			readIndex(index){
+				this.itemIndex = index;
+				this.showDetail = true;
+				this.detailMode = constantes.STATE_READ;
+				this.enableButtons = false;
+			},
+			updateIndex(index){
+				this.itemIndex = index;
+				this.showDetail = true;
+				this.enableButtons = false;
+				this.detailMode = constantes.STATE_UPDATE;
+			},		
+			changeTab(option){
+				this.menuChoice = option;
+				this.showDetail = false;
+			}
+
+		}
+	}
+</script>
+
+<style></style>
