113f3196affe5b73065c95b3c2cdf7ea37b59acd
juancarrascocic <juancarrascocic@gmail.com>
2017-09-01 17:48:44 +0200

    Completado esqueleto de cliente

---
+++ cliente/components/detail.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="Detail" class="detail-div">
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		components:{
+		},
+		data (){
+			return{
+			}
+		},
+		computed:{
+
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style></style>
---
+++ cliente/components/master.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="Master" class="master-div">
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		components:{
+		},
+		data (){
+			return{
+			}
+		},
+		computed:{
+
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style></style>
