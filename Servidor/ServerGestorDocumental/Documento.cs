00c58c3942134f4183f1c1584f70ef7dfa2c4ee2
JuanElias <juaneliaslaiseca@hotmail.com>
2017-09-05 17:58:02 +0200

    Cambio de tipo de variables

--- Servidor/ServerGestorDocumental/Documento.cs
+++ Servidor/ServerGestorDocumental/Documento.cs
@@ -10,10 +10,10 @@ namespace formulario
         public long Id { get; set; }
         public string Titulo { get; set; }
         public string Autor { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public DateTime FechaUltimoModificado { get; set; }
+        public string FechaCreacion { get; set; }
+        public string FechaUltimoModificado { get; set; }
         public float Tamanio { get; set; }
-        public string Tipo { get; set; }
+        public int Tipo { get; set; }
         public bool VersionFinal { get; set; }
         public bool SoloLectura { get; set; }
     }
