88d3b1ca89de32de96913bdd46f0aa19ef686bff
cursocic01 <cursocic01@outlook.com>
2017-09-06 16:46:57 +0200

    Arreglado el hover del detalle, ya no se pone como seleccionado todo el detalle

--- cliente/components/plantillasMaster.vue
+++ cliente/components/plantillasMaster.vue
@@ -1,58 +1,119 @@
 <template>
-<table class="table">
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Título</th>
-					<th>Última modificación</th>
-					<th>Tipo</th>
-					<th>Solo lectura</th>
-							<th> <a href=""> <i class="fa fa-user-plus" aria-hidden="true" v-on:click="newDetail()"></i> </a></th>
-						</tr>
-					</thead>			 
-					<tbody @click="" v-for="(item, index) in lista">
+	<div id="documentosMaster">
+		<table class="table">
+			<table class="table table-hover" role="tablist">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nombre</th>
+						<th>Autor</th>
+						<th>Tipo</th>
+						<th>Precio</th>
+						<th> <i class="fa fa-user-plus" aria-hidden="true" v-on:click="getNewDetail()"></i> </a></th>
+					</tr>
+				</thead>			
+				<tr  v-if="computeShowNewDetail">
+					<td colspan="6">
+						<detail @cancelDetail ="removeDetail" :currentId = "elegido" :state ="state" role="tabpanel" class="float-right"> </detail>
+					</td>
+				</tr>
+				<tbody @click="" v-for="(item, index) in lista">
 
-						<tr >
+					<tr v-on:click="renderDetail(item.Id)">
+						<th scope="row" v-model='index'>{{index}}</th>
+						<td>{{item.Titulo}}</td>
+						<td>{{item.Titulo}}</td>
+						<td>{{item.Tipo}}</td>
+						<td>{{item.SoloLectura}}</td>
 
-							<th scope="row" v-model='index'>{{index}}</th>
-							<td>{{item.Titulo}}</td>
-							<td>{{item.Titulo}}</td>
-							<td>{{item.Tipo}}</td>
-							<td>{{item.SoloLectura}}</td>		
+					</tr>
+					</tbody>
+					<tr  v-if="item.Id == elegido">
+						<td colspan="6">
+							<detail @makeGet= "recargarMaster" @cancelDetail = "removeDetail" :currentId = "elegido" :state = "state" role="tabpanel" class="float-right"> </detail>
+						</td>
+					</tr>
+							    
 
-						</tr>
-					</tbody>			    
-				</table>
-			</div>	    
-</table>
+			</table>
+			
+		</table>
+	</div>
 </template>
 <script type="text/javascript">
-import detail from './detail.vue'
+	import constantes from './constants.js';
+	import detail from './detail.vue'
 	export default{
-		name: "Plantillas",
+		name: "Documentos",
 		components:{
 			detail,
 		},
 		data (){
 			return{
-				lista: []
+				lista: [],
+				menuChoice:"Documentos",
+				state: "",
+				elegido : "",
+
 			}
 		},
 		computed:{
-
+			computeShowNewDetail(){
+				return this.state === constantes.STATE_NEW;
+			},
 		},
 		methods:{
+			makeGetListRequest(){
+				$.ajax({
+					url: constantes.BASE_URL + this.menuChoice,
+					method: "GET"
+				})
+				.done(this.submitGetListValues)
+				.fail(function(){
+					alert("Ha fallado la carga del objeto");
+				})
+			},
+			recargarMaster: function(){
+				this.removeDetail();
+				this.makeGetListRequest();
+			},
+			removeDetail: function(){
+				this.elegido = "";
+				this.state = "";
+			},
+			submitGetListValues: function(datos){
+				this.lista = datos;
+			},
 			emitEnableDetailEvent(read) {
 		      // Send the event on a channel () with a payload ()
 		      EventBus.$emit('enableDetail', this.read);
-			},
-			newDetail: function(index){
-				this.read= false;
-				this.emitEnableDetailEvent(this.read);
-			},			
-		}
-	}
+		  },
+		  getNewDetail: function(){
+		  	this.state = constantes.STATE_NEW;
+		  	this.elegido = "";
+				//this.emitEnableDetailEvent(this.read);
+			},	
+			renderDetail: function(index){
+				if(this.state == constantes.STATE_UPDATE){
+					if(this.elegido == index){
+						this.elegido ="";
+						this.state = "";
+					}
+					else {
+						this.elegido = index;
+					}
+				}
+				else{
+					this.state = constantes.STATE_UPDATE
+					this.elegido = index;
+				//this.showDetail == false ? this.showDetail = true :  this.showDetail = false;
+			}
 
+		},		
+	},
+	created(){
+		this.makeGetListRequest();
+	},
+}
 </script>
 <style type="text/css"></style>
